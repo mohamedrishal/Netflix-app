041ff58ba1bf665087289b0880a7d6ee45261c97
mohamedrishal <rishalmundekkat@gmail.com>
2023-09-27 19:43:25 +0530

    netflix design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Banner from './component/Banner';
+import Row from './component/Row';
+import requests from './requests';
+import Nav from './component/Nav';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Netflix App Loading...</h1>
+      <Nav />
+      <Banner fetchUrl={requests.fetchNetflixOriginals} />
+      <Row isPoster={true} title="Netflix" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending In India" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated Movies" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/tmdbAxiosInstance.js
@@ -0,0 +1,12 @@
+//creating instance
+import axios from "axios"
+
+
+const tmdbAxiosInstance = axios.create({
+
+    baseURL: "https://api.themoviedb.org/3"
+
+
+})
+
+export default tmdbAxiosInstance;
